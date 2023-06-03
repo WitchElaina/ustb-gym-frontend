a262f1dc6af7c109762720cd18d54002332d27d9
WitchElaina <msz13399173387@gmail.com>
2023-06-03 23:16:10 +0800

    Change base url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/gym/',
+  base: '/ustb-gym-frontend/',
   plugins: [vue()],
   resolve: {
     alias: {
