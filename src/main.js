fa411e8f5efdc80fcfb72ec52aed3d19da78ee8f
WitchElaina <msz13399173387@gmail.com>
2023-06-04 11:26:09 +0800

    Add admin register check

--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@ import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import App from './App.vue';
 import router from './router';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
 const app = createApp(App);
 
 app.use(router);
 app.use(ElementPlus);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
 
 app.mount('#app');
